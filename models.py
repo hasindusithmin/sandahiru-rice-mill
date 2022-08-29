43c1bab959fc26b7849ad1e3ccddf7620be70065
hasindusithmin <hasindusithmin64@gmail.com>
2022-08-29 15:49:57 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,15 @@
 
 from sqlmodel import SQLModel,Field
 from typing import Optional
+from datetime import datetime
+from pytz import timezone
+from enum import Enum
+
+
+class Bag(Enum):
+    SMALL = "5 KILO"
+    MEDIUM = "10 KILO"
+    LARGE = "25 KILO"
 
 
 class Stock(SQLModel,table=True):
@@ -10,3 +19,10 @@ class Stock(SQLModel,table=True):
     small:int = Field(default=None)
     medium:int = Field(default=None)
     large:int = Field(default=None)
+
+class Transaction(SQLModel,table=True):
+    id:Optional[int] = Field(default=None,primary_key=True)
+    date:Optional[datetime] = Field(default=datetime.now(tz=timezone("Asia/Colombo")))
+    bag:Bag = Field(nullable=False)
+    quantity:int = Field(nullable=False)
+    stock_id:Optional[int] = Field(default=None,foreign_key="stock.id")
