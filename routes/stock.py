47e1e1866ce3ab51adeee1b8acad6215a304d839
hasindusithmin <hasindusithmin64@gmail.com>
2022-08-29 17:17:27 +0530

    Update routes/stock.py & requirements.txt

--- routes/stock.py
+++ routes/stock.py
@@ -3,13 +3,14 @@ from fastapi import APIRouter,HTTPException
 from sqlmodel import Session,select
 from models import Stock
 from database import engine
+from typing import List
 
 router = APIRouter(
     prefix="/stock",
     tags=["Stock"]
 )
 
-@router.get("/",status_code=200)
+@router.get("/",status_code=200,response_model=List[Stock])
 async def get_stock_details():
     with Session(engine) as session:
         statement = select(Stock)
