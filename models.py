c23ba57903f1b1651d0e1a6abd781267f4d64560
hasindusithmin <hasindusithmin64@gmail.com>
2022-08-29 13:17:26 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,12 @@
 
+from sqlmodel import SQLModel,Field
+from typing import Optional
+
+
+class Stock(SQLModel,table=True):
+    id:Optional[int] = Field(default=None,primary_key=True)
+    name:str = Field(nullable=False)
+    unit:float = Field(nullable=False)
+    small:int = Field(default=None)
+    medium:int = Field(default=None)
+    large:int = Field(default=None)
